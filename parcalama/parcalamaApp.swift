01dc9142a31230380d020caf5311d11a368ea25a
YUSUFPELTEK <yusufpeltek0@gmail.com>
2023-04-17 04:37:10 +0300

    Initial Commit

---
+++ parcalama/parcalamaApp.swift
@@ -0,0 +1,17 @@
+//
+//  parcalamaApp.swift
+//  parcalama
+//
+//  Created by YUSUF on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct parcalamaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
